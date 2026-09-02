10f96864e35d00ccf009be6b6d451f1bd60a518f
matheus <matheus.gomes.bezerra@gmail.com>
2026-09-02 19:31:40 -0300

    organizacao dos diretorios e atividade da lista 1

---
+++ atividades/atividade1/questao7.py
@@ -0,0 +1,11 @@
+print("Cálculo de desconto:")
+
+porcentagem_desconto = 0.15
+valor_compra = 250.50
+valor_economizdo = valor_compra * porcentagem_desconto
+valor_com_desconto = valor_compra * porcentagem_desconto
+
+print("Valor original da compra:", valor_compra)
+print("Valor com desconto:", valor_com_desconto)
+print("Valor economizado: ", valor_economizdo)
+

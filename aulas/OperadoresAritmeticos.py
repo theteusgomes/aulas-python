a8fc02ad3243fd2bf9a35c62ca3717b54a7807cc
matheus <matheus.gomes.bezerra@gmail.com>
2026-09-01 21:41:15 -0300

    atividade operadores logicos

---
+++ aulas/OperadoresAritmeticos.py
@@ -0,0 +1,19 @@
+numero1 = 10
+numero2 = 20
+
+#soma
+soma = numero1 + numero2
+#subtração
+subtracao = numero1 - numero2
+#multiplicação
+multiplicacao = numero1 * numero2
+#divisão
+divisao = numero1 / numero2
+#resto
+resto = numero1 % numero2
+
+print("Soma dos Números:", soma)
+print("Subtração dos Números:", subtracao)
+print("Multiplicação dos Números:", multiplicacao)
+print("Divisão dos Números:", divisao)
+print("Resto:", resto)

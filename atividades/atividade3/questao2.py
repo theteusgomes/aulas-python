007656e764c24b03181d536ecebd00b5fcf7fb09
matheus <matheus.gomes.bezerra@gmail.com>
2026-09-02 21:40:44 -0300

    realizando as atividades

--- atividades/atividade3/questao2.py
+++ atividades/atividade3/questao2.py
@@ -2,9 +2,9 @@ quantidade_dia = int(input("quantas macas foram colhidas hoje?"))
 
 caixa_maca = 12
 
-producao_caixa =  quantidade_dia / caixa_maca
+producao_caixa =  quantidade_dia // caixa_maca
 
 resto = quantidade_dia % caixa_maca
 
-print(producao_caixa)
-print(resto)
+print("Foram produzidas:", producao_caixa, "caixas")
+print("Vão sobrar:", resto , "maçãs")

1c0a88991346edb6ac171ceeadf78be9e713d27e
matheus <matheus.gomes.bezerra@gmail.com>
2026-09-02 21:20:07 -0300

    realizando as atividades

--- atividades/atividade3/questao6.py
+++ atividades/atividade3/questao6.py
@@ -2,3 +2,6 @@ senha_cadastrada = 1234
 senha_digitada = int(input("Digite sua senha: "))
 acesso_liberado = senha_cadastrada == senha_digitada
 print("Acesso liberado?", acesso_liberado)
+
+"""estaca dando errado porque sem o Int ele compara uma strig
+com numero"""
